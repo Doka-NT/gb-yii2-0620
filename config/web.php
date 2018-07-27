67a9ac77220338b708d8da16c2985ccc5798d9ea
Soshnikov Artem <213036@skobka.com>
2018-07-27 22:33:18 +0500

    lesson-2 разбор ДЗ

--- config/web.php
+++ config/web.php
@@ -49,6 +49,7 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+            	'<controller>/<action>/<id:\d+>' => '<controller>/<action>',
             ],
         ],
     ],
@@ -68,7 +69,7 @@ if (YII_ENV_DEV) {
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['127.0.0.1', '::1', '*'],
     ];
 }
 
